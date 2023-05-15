5013604b0229b16aef778e96448687ff3e694e0a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-05-15 23:18:21 +0200

    Improving code maintainability

--- src/git.rs
+++ src/git.rs
@@ -1,6 +1,7 @@
 use git2::{Repository, Signature};
+use crate::utils::GitInfo;
 
-pub fn add_commit_push(repo_path: &str, commit_message: &str) -> Result<(), git2::Error> {
+pub fn add_commit_push(repo_path: &str, commit_message: &str, git_info: &GitInfo) -> Result<(), git2::Error> {
     let repo = Repository::open(repo_path)?;
 
     let mut index = repo.index()?;
@@ -10,20 +11,4 @@ pub fn add_commit_push(repo_path: &str, commit_message: &str) -> Result<(), git2
     index.write()?;
 
     let oid = index.write_tree()?;
-    let signature = Signature::now("Your Name", "you@example.com")?;
-    let parent_commit = find_last_commit(&repo)?;
-    let tree = repo.find_tree(oid)?;
-
-    // committing
-    repo.commit(Some("HEAD"), &signature, &signature, commit_message, &tree, &[&parent_commit])?;
-
-    // pushing
-    repo.find_remote("origin")?.push(&["refs/heads/master:refs/heads/master"], None);
-
-    Ok(())
-}
-
-fn find_last_commit(repo: &Repository) -> Result<git2::Commit, git2::Error> {
-    let obj = repo.head()?.resolve()?.peel(git2::ObjectType::Commit)?;
-    obj.into_commit().map_err(|_| git2::Error::from_str("Couldn't find commit"))
-}
+    let signature = Signature::now(&
