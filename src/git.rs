d6ac4cf9b6ab5b5ccad8ef7d1f7e65cfb4f95fc0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-05-16 00:22:02 +0200

    Updated code

--- src/git.rs
+++ src/git.rs
@@ -25,13 +25,14 @@ pub fn add_commit_push(repo_path: &str, commit_message: &str, git_info: &GitInfo
     };
 
     let mut callbacks = RemoteCallbacks::new();
-    callbacks.credentials(|_url, _username_from_url, _allowed_types| Ok(credentials.clone()));
+    callbacks.credentials(move |_url, _username_from_url, _allowed_types| Ok(credentials.clone()));
 
     let mut options = PushOptions::new();
     options.remote_callbacks(callbacks);
 
+    
     println!("Pushing changes to remote: {}", &git_info.remote_name);
-    remote.push(&[format!("refs/heads/{}:refs/heads/{}", git_info.branch_name, git_info.branch_name)], Some(&mut options))?;
+    remote.push(&[format!("refs/heads/{}:refs/heads/{}", git_info.branch_name, git_info.branch_name)], Some(&options))?;
 
     // pushing
     // repo.find_remote(&git_info.remote_name)?.push(&[format!("refs/heads/{}:refs/heads/{}", git_info.branch_name, git_info.branch_name)], None);
