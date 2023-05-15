f5e600f47ac8da106eb67bbe532713f15c86afbc
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-05-15 23:19:04 +0200

    Updated code

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::process;
 
 mod config;
 mod git;
+mod utils;
 
 #[tokio::main]
 async fn main() {
@@ -46,17 +47,29 @@ async fn main() {
     let commit_timer = config.commit_timer;
     let mut message_index = 0;
 
+    // Load git info from temp file, if it doesn't exist fetch and write to temp file
+    let git_info = if Path::new("gitzap_temp.json").exists() {
+        match utils::GitInfo::load_from_temp() {
+            Ok(gi) => gi,
+            Err(_) => {
+                eprintln!("Failed to load git info from temp file.");
+                process::exit(1);
+            }
+        }
+    } else {
+        let gi = utils::get_git_info();
+        match gi.write_to_temp() {
+            Ok(_) => gi,
+            Err(_) => {
+                eprintln!("Failed to write git info to temp file.");
+                process::exit(1);
+            }
+        }
+    };
+
     loop {
         // Wait for the timer duration
         sleep(Duration::from_secs(commit_timer as u64)).await;
 
         // Get the commit message
-        let commit_message = &commit_messages[message_index];
-        message_index = (message_index + 1) % commit_messages.len();
-
-        // Add, commit and push changes
-        if let Err(e) = git::add_commit_push(repo_path, commit_message) {
-            eprintln!("Failed to commit and push changes: {}", e);
-        }
-    }
-}
+        let commit_message = &commit_messages[message_index
