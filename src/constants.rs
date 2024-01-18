868da7e604e4e4d24919aa4718f6c31e50919c12
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-18 14:35:09 +0100

    🔧 Tuned performance parameters for a feature

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,2 @@
-use
-
-dirs::home_dir()
-
 /// Default configuration file.
-pub const DEFAULT_CONFIG_FILE: &str = "config.toml";
+pub const DEFAULT_CONFIG_FILE: &str = concat!(dirs::home_dir(), "config.toml");
