40d1b7e91b03c7462d83959a65128d9c774e8529
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-18 14:53:42 +0100

    📚 Updated an update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-mod cli;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    version = env!("CARGO_PKG_VERSION"),
+)]
+pub struct Cli {
+    #[clap(
+        short = 'c',
+        long = "config",
+        help = "Configuration file path",
+        default_value = "config.toml",
+        display_order = 1
+    )]
+    config: String,
+}
 
 #[tokio::main]
 async fn main() {
