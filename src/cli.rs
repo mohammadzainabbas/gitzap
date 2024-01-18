4cf5cfaa58c7c83df4253c3c37f58422381366ae
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-18 14:28:37 +0100

    🧱 Laid groundwork for some code

---
+++ src/cli.rs
@@ -0,0 +1,13 @@
+use clap::Parser;
+#[derive(Debug, Parser)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    version = env!("CARGO_PKG_VERSION"),
+)]
+struct Cli {
+    #[clap(short = 'c', long = "config", help = "Configuration file path", default_value = DEFAULT_CONFIG_FILE, display_order = 1)]
+    config: String,
+    #[clap(short = 'd', long = "database", help = "SQLite database path", default_value = DEFAULT_DATABASE_FILE, display_order = 2)]
+    database: String,
+}
