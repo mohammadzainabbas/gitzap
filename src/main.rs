c577a5488eb9874ef4a39775dcf8256f93cfd1c6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-23 18:04:24 +0100

    📦 Packaged a module

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,4 @@
-use clap::Parser;
 
-#[derive(Debug, Parser)]
-#[clap(
-    name = env!("CARGO_PKG_NAME"),
-    about = env!("CARGO_PKG_DESCRIPTION"),
-    version = env!("CARGO_PKG_VERSION"),
-)]
-pub struct Cli {
-    #[clap(
-        short = 'c',
-        long = "config",
-        help = "Configuration file path",
-        default_value = "config.toml",
-        display_order = 1
-    )]
-    config: String,
-
-    #[clap(
-        short = 'p',
-        long = "path",
-        help = "Path for git repo",
-        default_value = ".",
-        display_order = 2
-    )]
-    path: String,
-}
 
 #[tokio::main]
 async fn main() {
