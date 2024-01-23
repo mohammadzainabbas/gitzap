615227afe83e2edb58be57c482284b3360444bfc
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-23 18:14:38 +0100

    🔄 Improving code reusability across modules

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,4 @@
-use crate::Cli;
-
 #![allow(unused)]
 trait GitUtils {
     fn is_git_repo(path: String) -> bool;
-}
-
-impl GitUtils for Cli {
-    fn is_git_repo(path: String) -> bool {
-        todo!()
-    }
 }
