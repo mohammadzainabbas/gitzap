50b7402645a4db476038936a9e5e810cc30517fc
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-23 18:05:28 +0100

    ✅ Passed all tests for some updates

--- src/cli.rs
+++ src/cli.rs
@@ -1,29 +1,27 @@
-mod cli {
-    use clap::Parser;
+use clap::Parser;
 
-    #[derive(Debug, Parser)]
-    #[clap(
+#[derive(Debug, Parser)]
+#[clap(
     name = env!("CARGO_PKG_NAME"),
     about = env!("CARGO_PKG_DESCRIPTION"),
     version = env!("CARGO_PKG_VERSION"),
 )]
-    pub struct Cli {
-        #[clap(
-            short = 'c',
-            long = "config",
-            help = "Configuration file path",
-            default_value = "config.toml",
-            display_order = 1
-        )]
-        config: String,
+pub struct Cli {
+    #[clap(
+        short = 'c',
+        long = "config",
+        help = "Configuration file path",
+        default_value = "config.toml",
+        display_order = 1
+    )]
+    config: String,
 
-        #[clap(
-            short = 'p',
-            long = "path",
-            help = "Path for git repo",
-            default_value = ".",
-            display_order = 2
-        )]
-        path: String,
-    }
+    #[clap(
+        short = 'p',
+        long = "path",
+        help = "Path for git repo",
+        default_value = ".",
+        display_order = 2
+    )]
+    path: String,
 }
